9e0629985150df860444e9a8742cb67ac8e23e8e
MuraraAllan <allan.murara@gmail.com>
2017-04-09 12:34:41 -0300

    09/04/2017
    Removed html based movements and useless code.
    Added objects_handler.js - handle movements by prototype.
    Added texteditor - codemirror.

---
+++ FURBOT/visual/js/objects_handler.js
@@ -0,0 +1,149 @@
+//constants for direction handler, this should be instanced in a config file from server when we implement (soon enought to surprise :D) server-side language parameters (soon enought to surprise :D)
+const direita = 'direita';
+const esquerda = 'esquerda';
+const acima = 'acima';
+const abaixo = 'abaixo';
+const aquimesmo = 'aquimesmo';
+//instance world object, the mandatory function for object chain. The heritance will occur from this
+function worldobject(x,y,identifier,htmlelement)
+{
+   this.x = x;
+   this.y = y;
+   this.steps = 0;
+   this.class = identifier;
+   this.dom = htmlelement;
+   this.processing = 0;
+}
+worldobject.prototype.andarDireita = function() 
+{ 
+   if(this.processing == 1)
+   {
+      return false;
+   }
+   var isEnd = this.ehFim('direita');
+   if(isEnd == true)
+   {
+      return "Ei amigo, você esta tentando me levar para o fim do mundo???"
+   }
+   this.processing = 1;
+   var _this = this; 
+   setTimeout(function(){ _this.x++; $(_this.dom).appendTo("div[x="+_this.x+"][y="+_this.y+"]"); _this.processing = 0; }, 1000);
+   return;
+}
+worldobject.prototype.andarEsquerda = function() 
+{
+   if(this.processing == 1)
+   {
+      return false;
+   }
+   var isEnd = this.ehFim('esquerda');
+   if(isEnd == true)
+   {
+      return "Ei amigo, você esta tentando me levar para o fim do mundo???"
+   }
+   this.x --;
+   $(this.dom).appendTo("div[x="+this.x+"][y="+this.y+"]");
+};
+worldobject.prototype.andarAbaixo = function() 
+{
+   if(this.processing == 1)
+   {
+      return false;
+   }
+   var isEnd = this.ehFim(abaixo);
+   if(isEnd == true)
+   {
+      return "Ei amigo, você esta tentando me levar para o fim do mundo???"
+   }
+   this.y ++;
+   $(this.dom).appendTo("div[x="+this.x+"][y="+this.y+"]");
+};
+worldobject.prototype.andarAcima = function() 
+{
+   if(this.processing == 1)
+   {
+      return false;
+   }
+   var isEnd = this.ehFim(acima);
+   if(isEnd == true)
+   {
+      return "Ei amigo, você esta tentando me levar para o fim do mundo???"
+   }
+   this.y --;
+   $(this.dom).appendTo("div[x="+this.x+"][y="+this.y+"]");
+};
+
+//get currently object XY
+worldobject.prototype.getXY = function()
+{
+   if(this.processing == 1)
+   {
+      return false;
+   }
+   var currentcoords = { x: this.x , y: this.y };
+   return currentcoords
+}
+
+//check if direction is world end
+worldobject.prototype.ehFim = function(direction,obj)
+{
+   if(this.processing == 1)
+   {
+      return false;
+   }
+   var coords = tratadirecao(direction,this);
+   if ( coords == "invalid")
+   {
+      return "Direção invalida, As direções possíveis são : acima, direita, esquerda, abaixo!";
+   } 
+   var isEnd = true;
+   $("div[x="+coords.x+"][y="+coords.y+"]").each(function(i,t)
+   {
+      isEnd = false; 
+   })
+   return isEnd
+}
+
+function Furbot(x,y,identifier,htmlelement) {
+  worldobject.call(this,x,y,identifier,htmlelement); // call constructor-father
+}
+
+function Alien(x,y,identifier,htmlelement) {
+  worldobject.call(this,x,y,identifier,htmlelement); // call constructor-father
+}
+// Define prototype for objects, instance they in object chain. defining their constructor will set the attribs and defining their prototype will set the object chain functions
+Furbot.prototype = Object.create(worldobject.prototype);
+Furbot.prototype.constructor = Furbot;
+Alien.prototype = Object.create(worldobject.prototype);
+Alien.prototype.constructor = Alien;
+
+var aliens = [];
+var furbot;
+//when document loaded, create the objects using the object chain
+$( document ).ready(function() 
+{
+   //declaring single object called Furbot 
+   var domelement = document.getElementById('foobot'), context = domelement.getContext('2d');    
+   furbot = new Furbot(domelement.getAttribute('x'),domelement.getAttribute('y'),domelement.className,domelement);
+   base_image = new Image();
+   base_image.src = 'imgs/foobot.jpg';
+   base_image.onload = function()
+   {
+      context.drawImage(base_image, 0,0, 250, 250);
+   }
+   //iterate over all dom elements called .allien ( server heritance ) and create a array of objects called aliens 
+   $('.alien').each( function(i,t)
+   {
+      domelement = document.getElementById(t.id);
+      aliens[i] = new Alien(domelement.getAttribute('y'),domelement.getAttribute('x'),domelement.className,domelement);
+      var context = aliens[i].dom.getContext('2d');    
+      base_imagealien = new Image();
+      base_imagealien.src = 'imgs/alien.jpg';
+      base_imagealien.onload = function()
+      {
+        context.drawImage(base_imagealien, 0,0, 250, 250);
+      }
+   });
+
+
+});
--- FURBOT/visual/js/utils.js
+++ FURBOT/visual/js/utils.js
@@ -1,8 +1,8 @@
-function tratadirecao(direcao)
+function tratadirecao(direcao,obj)
 {
 	if ( direcao == 'acima' || direcao == 'esquerda' || direcao == 'direita' || direcao == 'abaixo' )
     {
-	   var coords = getXY(foobot);
+	   var coords = obj.getXY();
 	   switch(direcao)
 	   {
 	      case 'acima': coords.y --; break;
@@ -10,14 +10,14 @@ function tratadirecao(direcao)
 	      case 'direita': coords.x ++; break;
 	      case 'esquerda': coords.x --; break;
 	   }
-	   if(ehFim(coords))
-	   {
- 	      return "ehfim"
-	   }
+	//   if(ehFim(coords))
+	  // {
+ 	    //  return "ehfim"
+	   //}
        return coords;
     }
 
-    return "direcaoinvalida";
+    return "invalid";
 }
 
 function getXY(obj)
@@ -25,13 +25,38 @@ function getXY(obj)
 	var currentcoords = {x: $(obj).parent().attr('x'), y : $(obj).parent().attr('y') };
 	return currentcoords;
 }
-
 function sleep(ms,caller)
 {
 	window.setTimeout(function(){ oie('testes') }, ms);
 }
 
-function threadexterna(executa)
+function run_bot()
 {
-	
+	try {
+	console.log(eval(texteditor.getValue()))
+} catch (e) {
+ console.log(e)    
+}
+
+}
+
+function ehFim(direcao)
+{
+   return furbot.ehFim(direcao);
+}
+function andarDireita()
+{
+   return furbot.andarDireita();
+}
+function andarEsquerda()
+{
+   return furbot.andarEsquerda();
+}
+function andarAbaixo()
+{
+   return furbot.andarAbaixo();
+}
+function andarAcima()
+{
+   return furbot.andarAcima();
 }
